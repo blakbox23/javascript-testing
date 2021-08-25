43209039b8b4e9c39bcbaacb48257ae9199f1622
Peter MX <36844547+blakbox23@users.noreply.github.com>
2021-08-25 14:06:31 +0300

    stringLength first test

--- stringLength.test.js
+++ stringLength.test.js
@@ -1,2 +1,7 @@
+const { expect } = require('@jest/globals');
 const { stringLength } = require('./main');
 
+test('should output length of string', () => {
+    const count = stringLength('one');
+    expect(count).toBe(3);
+});
